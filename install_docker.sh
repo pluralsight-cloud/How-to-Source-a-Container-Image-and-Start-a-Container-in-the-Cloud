e9267b633ece12de10a2c114c37b16cbf05ff5a6
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2023-03-07 08:01:46 -0800

    Adding first version

---
+++ install_docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Update the apt package index and install some required packages
+
+#lsof /var/lib/dpkg/lock-frontend
+LSOF_STATUS=0
+until [ $LSOF_STATUS -ne 0 ];
+do
+        echo locked
+        sleep 2
+        lsof /var/lib/dpkg/lock-frontend
+        LSOF_STATUS=$?
+done
+
+apt-get update
+apt install -y ca-certificates curl gnupg lsb-release
+
+#Add Docker GPG key
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+#Set up the repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+#Install Docker Engine
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#Add cloud_user to docker group
+usermod -aG docker cloud_user
+
+touch /tmp/docker_done
