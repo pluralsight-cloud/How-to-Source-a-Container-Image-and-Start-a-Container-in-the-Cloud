4239d90c790a30b36609d5338085d63ba3e26c6e
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2023-03-21 10:48:56 -0700

    Container start up for lab

--- start_web-1.sh
+++ start_web-1.sh
@@ -7,3 +7,5 @@ docker run -d --name web-1 -p 8081:80 nginx
 docker cp /etc/apt/sources.list web-1:/etc/apt/sources.list
 docker exec web-1 bash -c "apt-get update"
 docker exec web-1 bash -c "apt install -y vim"
+
+touch /tmp/web-1_done
