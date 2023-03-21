b32021047119a896443d2ccffbd4b106356501ee
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2023-03-21 10:43:57 -0700

    Container start up for lab

---
+++ start_web-1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Start the initial container
+docker run -d --name web-1 -p 8081:80 nginx
+
+#Install vim in the container
+docker cp /etc/apt/sources.list web-1:/etc/apt/sources.list
+docker exec web-1 bash -c "apt-get update"
+docker exec web-1 bash -c "apt install -y vim"
