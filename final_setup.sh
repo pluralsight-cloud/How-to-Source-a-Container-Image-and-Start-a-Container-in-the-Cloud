7502678e8112b6e2a005213b98b5ed2d21022c6c
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2023-03-23 14:13:22 -0700

    removed some image lines

--- final_setup.sh
+++ final_setup.sh
@@ -4,9 +4,9 @@
 docker run -dt --name checker debian:buster
 docker cp /etc/apt/sources.list checker:/etc/apt/
 docker cp cloud_pass checker:/tmp/
-docker cp check_final.sh checker:/tmp/
+#docker cp check_final.sh checker:/tmp/
 docker exec checker bash -c "apt-get update && apt install -y -qq vim ssh sshpass wget"
-docker exec checker bash -c "wget https://github.com/pluralsight-cloud/How-to-Source-a-Container-Image-and-Start-a-Container-in-the-Cloud/raw/main/final_setup.sh -P /tmp/"
+docker exec checker bash -c "wget https://github.com/pluralsight-cloud/How-to-Source-a-Container-Image-and-Start-a-Container-in-the-Cloud/raw/main/check_final.sh -P /tmp/"
 docker stop checker
 docker commit checker server-1:5000/checker:latest
 docker rm checker
