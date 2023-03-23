aca8c5ae060fbaf07cbd66eac4f3449788684921
Justin Burrell <justin.burrell@LA-C02WW3B1HTD6.local>
2023-03-23 13:55:51 -0700

    removed some image lines

--- final_setup.sh
+++ final_setup.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-CLOUDPASS=`cat /home/cloud_user/cloud_pass`
 #Start container
-echo "$CLOUDPASS" | docker login -u cloud_user --password-stdin server-1:5000
 docker run -dt --name checker debian:buster
 docker cp /etc/apt/sources.list checker:/etc/apt/
 docker cp cloud_pass checker:/tmp/
+docker cp check_final.sh checker:/tmp/
 docker exec checker bash -c "apt-get update && apt install -y -qq vim ssh sshpass wget"
 docker exec checker bash -c "wget https://github.com/pluralsight-cloud/How-to-Source-a-Container-Image-and-Start-a-Container-in-the-Cloud/raw/main/final_setup.sh -P /tmp/"
-docker exec checker bash -c "chmod +x /tmp/check_final.sh"
 docker stop checker
 docker commit checker server-1:5000/checker:latest
-docker push server-1:5000/checker:latest
 docker rm checker
-docker rmi debian:buster server-1:5000/checker:latest
-docker logout server-1:5000
+docker rmi debian:buster 
+docker run -t --name checker server-1:5000/checker:latest /tmp/check_final.sh
